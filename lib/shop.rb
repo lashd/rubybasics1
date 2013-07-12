eb4bdc43bb629ea52e6f01f981043bdd5d3e3181
lashd <leon_a_d@yahoo.com>
2013-07-12 15:07:53 +0100

    the problem

---
+++ lib/shop.rb
@@ -0,0 +1,17 @@
+# price list 
+# orange = 10p apple = 20p bread = £1.10 tomato = 25p cereal = £2.34
+shopping_list=<<LIST
+list
+orange
+apple
+apple
+orange
+tomato
+cereal
+bread
+orange
+tomato
+LIST
+
+
+puts shopping_list
