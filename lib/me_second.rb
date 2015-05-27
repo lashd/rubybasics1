5eb20b4c90abaad1c5fe50cd41cc7fd5a945015a
Leon Davis <leon_a_d@yahoo.com>
2015-05-27 12:00:49 +0100

    Create me_second.rb

---
+++ lib/me_second.rb
@@ -0,0 +1,4 @@
+price_list = "orange = 10p apple = 20p bread = £1.10 tomato = 25p cereal = £2.34"
+shopping_list = [:orange, :apple, :apple, :cereal, :bread]
+
+#TODO - print out in pounds and pence the total for the shopping list
